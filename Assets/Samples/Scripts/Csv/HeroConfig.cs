5a7d3c88a6baab7f56078cc152ad750e423c5448
Haivd <haivd@zitga.com.vn>
2023-11-30 15:30:03 +0700

    Update Enum example

--- Assets/Samples/Scripts/Csv/HeroConfig.cs
+++ Assets/Samples/Scripts/Csv/HeroConfig.cs
@@ -3,10 +3,19 @@ using Sirenix.OdinInspector;
 
 namespace Csv
 {
+    public enum HeroType
+    {
+        None,
+        Warrior,
+        Ranger,
+        Healer,
+    }
+    
     [Serializable]
     public class HeroConfigItem
     {
         public int id;
+        public HeroType type;
         public float health;
         public float armor;
     }
